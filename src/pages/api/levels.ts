d924233b56ff3c83b0d7f4195085d95afa00305e
DrYonatan <talagin11@gmail.com>
2025-02-16 20:51:29 +0200

    Update levels.ts

--- src/pages/api/levels.ts
+++ src/pages/api/levels.ts
@@ -1,8 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { AppDataSource, connectDB } from "../../../ormconfig";
+import { AppDataSource, connectDB } from "../../../../ormconfig";
 import { Level } from "@/entities/level";
-
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,25 +9,8 @@ export default async function handler(
 ) {
   await connectDB();
 
-  try {
-    if (req.method === "GET") {
-
-      const { date } = req.query;
-  
-      if (date) {
-        const level = await AppDataSource.getRepository(Level).findOne({
-          where: { date: new Date(date as string) },
-        });
-        if (!level) return res.status(404).json({ message: "Level not found" });
-        return res.status(200).json(level);
-      }
-  
-      const levels = await AppDataSource.getRepository(Level).find();
-      return res.status(200).json(levels);
-    }
-  } catch (error) {
-    
+  if (req.method === "GET") {
+    const levels = await AppDataSource.getRepository(Level).find();
+    return res.status(200).json(levels);
   }
-  
 }
-
