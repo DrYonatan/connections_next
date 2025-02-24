edce18256084a98536b81d8db4e87b36b3e6375e
DrYonatan <talagin11@gmail.com>
2025-02-24 15:32:39 +0200

    Update ormconfig.ts

--- src/pages/api/ormconfig.ts
+++ src/pages/api/ormconfig.ts
@@ -7,7 +7,7 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.DB_PORT || '5432'),
+  port: parseInt('5432'),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
